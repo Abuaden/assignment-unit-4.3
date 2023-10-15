e02047c148450591743bdcfc058b410557046e4f
Abubakar Aden <abubakar.aden978@gmail.com>
2023-10-15 16:04:20 -0500

    Assignment completed

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,29 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+function addItem (item){
+basket.push(item);
+return true;
+}
 
+addItem('MacNcheese');
+addItem('Hotdog');
+console.log(basket);
+console.log('True:', addItem('MacNcheese'));
 
+function listItems(){
+    for(let item of basket){
+        console.log(item);
+    }
+}
+
+function empty(){
+    basket=[]
+}
+empty();
+console.log('Basket after emptying:', basket);
 
 
 
